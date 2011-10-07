ee63692b184deffb9e6b730ac2765d8f34c9521d
Julio Protzek <julioprotzek@gmail.com>
2011-10-07 10:40:36 -0300

    rails zerado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagamento::Application.config.secret_token = 'e481ca133853ac4ccf4417dbfc01c60c7cbe39d96059f79d340c3d8cbf0086e86055c7398fea3939ccc94e499e087b8242e2fc61df517b5fb07fca87f3113491'
